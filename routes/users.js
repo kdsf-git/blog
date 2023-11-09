c30f52f43d0c59135a70ff00f72834ffb7562c92
DrDemagol <amon-maier@t-online.de>
2023-11-09 23:31:21 +0100

    updated users.json

--- routes/users.js
+++ routes/users.js
@@ -2,36 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user'); // Assuming you have a User model
 
-// Register Page
-router.get('/register', (req, res) => {
-    res.render('register');
-});
-
-// Handle Registration (form submission)
-router.post('/register', (req, res) => {
-    const { username, email, password, bio, profilePicture } = req.body;
-    // Implement user registration logic and validation here
-    // Hash the password and create a new user record
-    // Redirect to the login page or user profile after successful registration
-});
-
-// Login Page
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
-// Handle Login (form submission)
-
-// router.post('/login', (req, res, next) => {
-//     passport.authenticate('local', {
-//         successRedirect: '/profile',
-//         failureRedirect: '/login',
-//         failureFlash: true
-//     })(req, res, next);
-// });
-
 // Profile Page (requires authentication)
-router.get('/profile', ensureAuthenticated, (req, res) => {
+router.get('/profile', (req, res) => {
     res.render('profile', { user: req.user });
 });
 
