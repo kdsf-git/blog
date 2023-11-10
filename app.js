f9e0f979d2d4690cb9a32f1d233ac1058b72497c
kdsf <fernandesk1703@gmail.com>
2023-11-11 00:38:19 +0100

    removed unnecessary db connection

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 // Sample code structure for the backend
 const express = require('express');
-const mysql2 = require('mysql2');
 const session = require('express-session');
 
 const app = express();
 
 // Middlewares, configurations, and routes setup
 app.set("view engine", 'ejs');
-
-// Connect to MySQL
-var con = mysql2.createConnection({
-    host: "192.168.0.12",
-    user: "app",
-    password: "dbpassword",
-    database: "db"
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
 
 // Implement the key features with routes, controllers, models, and views
 // Define routes
