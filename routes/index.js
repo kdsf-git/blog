e1c47738b948f8ce347cd6f20c1895347701ebe9
kdsf <fernandesk1703@gmail.com>
2023-11-11 20:45:46 +0100

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post.js');
 
-function getPostsByDate() {
+async function getPostsByDate() {
 	return await Post.findAll({
 		order: [
 			['date', 'DESC']
