155e0709d969706a9d85fde63121c8dcd09d1015
kdsf <fernandesk1703@gmail.com>
2023-11-11 20:18:45 +0100

    updated index route

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/post.js');
 
-// Database integration
-const posts = [
-    { id: 1, title: 'First Blog Post', content: 'This is the content of the first post.' },
-    { id: 2, title: 'Second Blog Post', content: 'This is the content of the second post.' },
-    { id: 3, title: 'Third Blog Post', content: 'This is the content of the third post.' }
-];
+function getPostsByDate() {
+	return Post.findAll({
+		order: [
+			['date', 'DESC']
+		]
+	});
+}
 
 // Routes
 router.get('/', (req, res) => {
-    res.render('index', { posts });
+    res.render('index', { getPostsByDate() });
 });
 
 // Start the server
