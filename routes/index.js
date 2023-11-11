6591ba3e7a8cf917ebffefa58ceaf2e144c39a08
kdsf <fernandesk1703@gmail.com>
2023-11-11 20:35:45 +0100

    bugfix

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,19 @@ const router = express.Router();
 const Post = require('../models/post.js');
 
 function getPostsByDate() {
-	return Post.findAll({
+	let res = [];
+	Post.findAll({
 		order: [
 			['date', 'DESC']
 		]
+	}).then((result) => {
+		res = result;
 	});
 }
 
 // Routes
 router.get('/', (req, res) => {
     const posts = getPostsByDate();
-    console.log(posts);
     res.render('index', { posts });
 });
 
