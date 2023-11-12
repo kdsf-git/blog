b71578b3b8baeea8d0f6629603f39dfd641a606a
kdsf <fernandesk1703@gmail.com>
2023-11-12 01:36:05 +0100

    updated login route

--- routes/login.js
+++ routes/login.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+const am = require('../models/authManager.js');
 
 // Login Page
 router.get('/', (req, res) => {
     const error = req.query.error;
-    res.render('login', { error });
+    am.getUserFromSession(req.session).then(username => {
+    	if(username) {
+	    res.redirect('/user/' + username);
+	} else {
+	    res.render('login', { error });
+	}
+    });
 });
 
 // Handle Login (form submission)
 router.post('/', (req, res) => {
-    const { username, password } = req.body;
-
-    // Implement your custom logic here to check if the username and password are valid
-    // You can replace this with your own authentication logic
-
-    if (username === 'exampleUser' && password === 'password123') {
-        // Redirect to a success page or the user's profile
-        res.redirect('/success');
-    } else {
-        // Redirect back to the login page with an error message
-        res.redirect('/login?error=1');
-    }
+	const { email, password } = req.body;
+	am.login(email, password).then(sid => {
+		if(sid) {
+			res.session.sessionId = sid;
+			res.redirect('/');
+		} else {
+			res.redirect('/login?error=1');
+		}
+	}
 });
 
 module.exports = router;
