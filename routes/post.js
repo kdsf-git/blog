f692b5f2e004a98e67ba096754c8250aa6174230
kdsf <fernandesk1703@gmail.com>
2023-11-12 11:07:56 +0100

    removed unnecessary code

--- routes/post.js
+++ routes/post.js
@@ -99,38 +99,17 @@ router.get('/:postId', (req, res) => {
 
 // Create a new blog post (form submission)
 router.post('/create', (req, res) => {
-    try {
-        const { title, content, tags } = req.body;
-        const newPost = new Post({ title, content, tags, author: req.user.id }); // Assuming user is authenticated
-        await newPost.save();
-        res.redirect('/');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+
 });
 
 // Update an existing blog post (form submission)
 router.post('/:postId/edit', (req, res) => {
-    try {
-        const { title, content, tags } = req.body;
-        const post = await Post.findByIdAndUpdate(req.params.postId, { title, content, tags }, { new: true });
-        res.redirect(`/posts/${post._id}`);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+
 });
 
 // Delete a blog post
 router.post('/:postId/delete', (req, res) => {
-    try {
-        await Post.findByIdAndRemove(req.params.postId);
-        res.redirect('/');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+
 });
 
 router.post('/:postId/kudos', (req, res) => {
