9aef4a4b74337f5d52a7938032456aaa4532728a
DrDemagol <amon-maier@t-online.de>
2023-11-12 17:27:33 +0100

    updated UI

--- routes/search.js
+++ routes/search.js
@@ -12,4 +12,29 @@ router.get('/', (req, res) => {
 router.post('/', bodyParser.urlencoded(), (req, res) => {
 });
 
+// GET route for displaying the search form
+router.get('/search', (req, res) => {
+	// Render the search form view (search.ejs)
+	res.render('search', {
+
+	});
+});
+
+// POST route for handling the search form submission
+router.post('/search', async (req, res) => {
+	try {
+		const { query, searchBy, keywords, sortBy, order } = req.body;
+
+		// search using parameters
+		const results = await searchPosts(query, searchBy, keywords, sortBy, order);
+
+		// search results view with retrieved results
+		res.render('search', { results, query, searchBy, keywords, sortBy, order });
+	} catch (error) {
+		// Handle errors
+		console.error(error);
+		res.render('error', { error });
+	}
+});
+
 module.exports = router;
